9ca8ef0781c0dfeb64a9f1cbc2b2666bc9a7122a
dsdhumale <d.sdhumale@gmail.com>
2022-12-13 07:39:00 +0530

    [Devendra]Added : Ability to calculate daily wages of Employee

--- EmployeeWage_Computation.js
+++ EmployeeWage_Computation.js
@@ -1,13 +1,19 @@
 console.log("Welcome in Employee Wage Computation Program....");
-var workingShift;
-(function (workingShift) {
-    workingShift[workingShift["isFULL_TIME"] = 1] = "isFULL_TIME";
-    workingShift[workingShift["isAbsent"] = 0] = "isAbsent";
-})(workingShift || (workingShift = {}));
-var option = Math.floor(Math.random() * 2);
-if (option == workingShift.isFULL_TIME) {
+var constants;
+(function (constants) {
+    constants[constants["isFULL_TIME"] = 1] = "isFULL_TIME";
+    constants[constants["isAbsent"] = 0] = "isAbsent";
+    constants[constants["wage_per_hr"] = 20] = "wage_per_hr";
+})(constants || (constants = {}));
+var empHrs;
+var option = Math.floor(Math.random() * 3);
+if (option == constants.isFULL_TIME) {
     console.log("Employee is present");
+    empHrs = 8;
 }
 else {
     console.log("Employee is absent");
+    empHrs = 0;
 }
+var dailyWage = empHrs * constants.wage_per_hr;
+console.log("Employee wage is ", dailyWage);
--- EmployeeWage_Computation.ts
+++ EmployeeWage_Computation.ts
@@ -1,14 +1,19 @@
 console.log("Welcome in Employee Wage Computation Program....");
 
-enum workingShift {
+enum constants {
     isFULL_TIME = 1,
-    isAbsent = 0
+    isAbsent = 0,
+    wage_per_hr = 20
 }
-
-let option = Math.floor(Math.random() * 2);
-if (option == workingShift.isFULL_TIME) {
+let empHrs;
+let option = Math.floor(Math.random() * 3);
+if (option == constants.isFULL_TIME) {
     console.log("Employee is present");
+    empHrs = 8;
 }
 else {
     console.log("Employee is absent");
+    empHrs = 0;
 }
+let dailyWage = empHrs * constants.wage_per_hr;
+console.log("Employee wage is ", dailyWage);
