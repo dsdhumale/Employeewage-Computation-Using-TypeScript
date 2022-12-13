7172ad7679829f44b19992b56b2f74bf2b87fe5d
dsdhumale <d.sdhumale@gmail.com>
2022-12-13 07:45:27 +0530

    [Devendra]Added : Ability to calculate daily wages for Part Tome Employee

--- EmployeeWage_Computation.js
+++ EmployeeWage_Computation.js
@@ -4,12 +4,17 @@ var constants;
     constants[constants["isFULL_TIME"] = 1] = "isFULL_TIME";
     constants[constants["isAbsent"] = 0] = "isAbsent";
     constants[constants["wage_per_hr"] = 20] = "wage_per_hr";
+    constants[constants["isPART_TIME"] = 2] = "isPART_TIME";
 })(constants || (constants = {}));
 var empHrs;
 var option = Math.floor(Math.random() * 3);
 if (option == constants.isFULL_TIME) {
     console.log("Employee is present");
     empHrs = 8;
+}
+else if (option == constants.isPART_TIME) {
+    console.log("Employee is working as Part Time Employee");
+    empHrs = 4;
 }
 else {
     console.log("Employee is absent");
--- EmployeeWage_Computation.ts
+++ EmployeeWage_Computation.ts
@@ -3,13 +3,18 @@ console.log("Welcome in Employee Wage Computation Program....");
 enum constants {
     isFULL_TIME = 1,
     isAbsent = 0,
-    wage_per_hr = 20
+    wage_per_hr = 20,
+    isPART_TIME = 2
 }
 let empHrs;
 let option = Math.floor(Math.random() * 3);
 if (option == constants.isFULL_TIME) {
     console.log("Employee is present");
     empHrs = 8;
+}
+else if(option == constants.isPART_TIME){
+    console.log("Employee is working as Part Time Employee");
+    empHrs = 4;
 }
 else {
     console.log("Employee is absent");
