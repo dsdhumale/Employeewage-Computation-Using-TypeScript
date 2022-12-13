15615a87827a4acd95278381d47d0f33604d116e
dsdhumale <d.sdhumale@gmail.com>
2022-12-13 07:29:20 +0530

    [Devendra]Added : Ability to check Employee Attendence

--- EmployeeWage_Computation.js
+++ EmployeeWage_Computation.js
@@ -1,1 +1,13 @@
 console.log("Welcome in Employee Wage Computation Program....");
+var workingShift;
+(function (workingShift) {
+    workingShift[workingShift["isFULL_TIME"] = 1] = "isFULL_TIME";
+    workingShift[workingShift["isAbsent"] = 0] = "isAbsent";
+})(workingShift || (workingShift = {}));
+var option = Math.floor(Math.random() * 2);
+if (option == workingShift.isFULL_TIME) {
+    console.log("Employee is present");
+}
+else {
+    console.log("Employee is absent");
+}
--- EmployeeWage_Computation.ts
+++ EmployeeWage_Computation.ts
@@ -1,1 +1,14 @@
 console.log("Welcome in Employee Wage Computation Program....");
+
+enum workingShift {
+    isFULL_TIME = 1,
+    isAbsent = 0
+}
+
+let option = Math.floor(Math.random() * 2);
+if (option == workingShift.isFULL_TIME) {
+    console.log("Employee is present");
+}
+else {
+    console.log("Employee is absent");
+}
